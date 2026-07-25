ca0243d13f95d93743a7913c4dec78e519cd704c
gytwo <15279172411@163.com>
2026-07-25 17:29:52 +0800

    feat(bottombar): add overlap toggle for reader view

--- qui_utils.lua
+++ qui_utils.lua
@@ -295,6 +295,7 @@ function Utils.getDefaultKeys(module_key)
             "qa_bb_transparent", "qa_bb_settings_on_hold", "qa_bb_button_hold_edit",
             "qa_bb_bg_color", "qa_bb_fg_color", "qa_bb_inactive_color",
             "qa_bb_accent_color", "qa_bb_labels", "qa_bb_tabs", "qa_bb_reader_enabled",
+            "qa_bb_overlap",
         },
         qa_common = {
             "qa_common_tab_icon", "qa_common_custom_list", "qa_common_custom",
