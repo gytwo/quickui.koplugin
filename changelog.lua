dac7be18eecc12126d197552a7df3996e8e77c6d
gytwo <15279172411@163.com>
2026-09-07 14:57:24 +0800

    Update changelog for version 1.0.5

--- changelog.lua
+++ changelog.lua
@@ -68,6 +68,6 @@ return {
 },
 ["1.0.5"] = {
     "Fix: prevent layout corruption when rotating screen",
-    "Added bottombar to history/collections/coll_list",
+    "Added bottombar to history/collections/favorites",
 },
 }
