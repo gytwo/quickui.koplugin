35a1539399e07b294afeb4a2ac84c168f80e85c8
gytwo <15279172411@163.com>
2026-09-16 15:09:19 +0800

    Add improvement for bottom bar show/hide experience

--- changelog.lua
+++ changelog.lua
@@ -80,5 +80,6 @@ return {
     "Bottom bar: fix missing bottom bar after screen rotation on Android",
     "Bottom bar: fix missing bottom bar when entering History/Collections from another fullscreen view",
     "Bottom bar: fix crash in History/Collections when handling tabs (add/move/sort/etc.)",
+    "Bottom bar: improve bottom bar show/hide experience in ReaderUI",
     },
 }
