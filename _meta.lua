2e581fa9af3bc8c8a719d0649c56244a42a2e6d3
gytwo <15279172411@163.com>
2026-09-26 21:15:12 +0800

    Bump version from 1.1.1 to 1.2.0

--- _meta.lua
+++ _meta.lua
@@ -22,6 +22,6 @@ return {
     fullname = _("QuickUI"),
     plugin_id = "quickui_plugin",
     description = _([[QuickUI: Quick Actions, Cover Visuals, Cloze Mode, Header & Footer — more efficient KOReader]]),
-    version = "1.1.1",
+    version = "1.2.0",
     author = "gytwo",
 }
